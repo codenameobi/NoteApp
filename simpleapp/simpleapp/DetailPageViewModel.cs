5f3b57acf8a90757b312dcdaa3a9b1e315c65811
admin <tobynwude@gmail.com>
2022-05-26 23:06:35 +0100

    push to github

---
+++ simpleapp/simpleapp/DetailPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace simpleapp
+{
+	public class DetailPageViewModel : INotifyPropertyChanged
+	{
+
+        public DetailPageViewModel(string note)
+		{
+			DismissPageCommand = new Command(async () =>
+			{
+				await Application.Current.MainPage.Navigation.PopAsync();
+
+				//modal navigation
+				//await Application.Current.MainPage.Navigation.PopModalAsync();
+			});
+
+			NoteText = note;
+		}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+		string noteText;
+
+		public string NoteText
+		{
+			get => noteText;
+
+			set
+			{
+				noteText = value;
+
+				var args = new PropertyChangedEventArgs(nameof(NoteText));
+
+				PropertyChanged?.Invoke(this, args);
+			}
+		}
+
+		public Command DismissPageCommand { get; }
+	}
+}
---
+++ simpleapp/simpleapp/MainPageViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace simpleapp
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            AllNotes = new ObservableCollection<string>();
+
+            SelectedNoteChangedCommand = new Command(async () =>
+            {
+                var detailVM = new DetailPageViewModel(SelectedNote);
+
+                var detailPage = new DetailPage();
+
+                detailPage.BindingContext = detailVM;
+
+                await Application.Current.MainPage.Navigation.PushAsync(detailPage);
+            });
+
+            EraseCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+
+            SaveCommand = new Command(() =>
+            {
+                
+                AllNotes.Add(TheNote);
+                TheNote = string.Empty;
+            });
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string selectedNote;
+
+        public string SelectedNote
+        {
+            get => selectedNote;
+
+            set
+            {
+                selectedNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(selectedNote));
+
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+        string theNote;
+
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveCommand { get; }
+        public Command EraseCommand { get; }
+        public Command SelectedNoteChangedCommand { get; }
+    }
+}
+
